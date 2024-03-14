5d5bd71b8fe9c76851d47f2634e9f334d6bea09e
[h584903] <[h584903@stud.hvl.no]>
2024-03-14 14:25:01 +0100

    Lagt til litt forskjellige filer og har startet på default layout

---
+++ components/header.vue
@@ -0,0 +1,44 @@
+<template>
+  <header class="header">
+    <div class="wrapper">
+    
+    </div>
+  </header>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+header {
+  font-size: 15px;
+  min-height: var(--headerHeight);
+  position: fixed;
+  background-color: var(--background);
+  width: 100%;
+  left: 0;
+  top: 0;
+  z-index: 700;
+}
+
+.wrapper {
+  margin: 0 auto;
+  height: 4.5rem;
+  width: 100%;
+  display: flex;
+  justify-content:space-between;
+  align-items: center;
+}
+
+@media (max-width: 1200px) {
+
+}
+
+@media (min-width: 1201px) {
+  .wrapper {
+      width: 80%;
+  }
+
+}
+
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,23 @@
 <template>
-  <slot />
+  <header />
+  <div class="content">
+    <sideBar />
+    <slot />
+  </div>
 </template>
+<style>
+
+body {
+    min-height: 100vh;
+    padding: 0;
+    margin: 0;
+    display: flex;
+    flex-direction: column;
+}
+
+.content {
+    padding-top: var(--headerHeight);
+    flex: 1;
+}
+
+</style>
