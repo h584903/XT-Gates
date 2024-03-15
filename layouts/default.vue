0eb40464df501af701fb20cc23b86575d2b2315c
Petter Tesdal <h584903@stud.hvl.no>
2024-03-15 09:34:55 +0100

    Fikset litt opp på koden og la til en mer representativ farge på NAV

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,23 +1,21 @@
 <template>
   <Header />
-  <sideBar />
   <div class="content">
+    <sideBar />
     <slot />
   </div>
 </template>
-<style>
+<style scoped>
 
 body {
     min-height: 100vh;
     padding: 0;
     margin: 0;
-    display: flex;
-    flex-direction: column;
 }
 
 .content {
     padding-top: var(--headerHeight);
-    flex: 1;
+    display: flex; /* Flexbox for the 2 items sidebar, and the rest of the content */
 }
 
 </style>
