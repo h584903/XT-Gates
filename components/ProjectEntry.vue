d956288772c9b49ba32699d1743fcda20de368e0
Petter Tesdal <h584903@stud.hvl.no>
2024-03-18 16:10:15 +0100

    Enkel utføring av DateEntry, tar imot en rekke objekter med datoer og representerer de på måten lokalet deres forventer det

--- components/ProjectEntry.vue
+++ components/ProjectEntry.vue
@@ -1,17 +1,31 @@
+<script setup>
+  import { ref } from 'vue'
+  
+  const dates = ref([
+    '2024-03-18',
+    '2024-03-19',
+    '2024-03-28'
+  ])
+
+  const sortDates = () => {
+    dates.value.sort((a, b) => new Date(a) - new Date(b))
+  }
+</script>
+
 <template>
   <!-- Følger prototypen til figma -->
-  <div class="wrapper">
+  <div class="list" v-for="(dateString, index) in dates" :key="inex">
   <span>Example Project</span>
   <ProgressBar />
-  <DateEntry />
-  <DateEntry />
+  <DateEntry :dateString="dateString"/>
+  <DateEntry :dateString="dateString"/>
   <PlanStatus />
   <PersonInCharge />
   </div>
 </template>
 <style>
 
-.wrapper {
+.list {
   display: flex;
   justify-content: space-evenly;
 }
