d8504ba5b973cbdc08252b97c7137293813ff090
h599017 <89259042+h599017@users.noreply.github.com>
2024-03-19 16:34:43 +0100

    endret listen over prosjekter til array

--- components/ProjectList.vue
+++ components/ProjectList.vue
@@ -1,20 +1,27 @@
 <template>
-  <div class="list-wrapper">
-    <ProjectEntry />
-    <hr class="solid">
-    <ProjectEntry />
-    <hr class="solid">
-    <ProjectEntry />
-    <hr class="solid">
-    <ProjectEntry />
-    <hr class="solid">
-    <ProjectEntry />
+  <div class="list-wrapper" v-for="entry in prosjektliste">
+    <ProjectEntry :entryData="entry"/>
     <hr class="solid">
   </div>
 </template>
-<script>
+
+
+<script setup>
+import ProjectEntry from './ProjectEntry.vue';
+
+const prosjektliste = [];
+
+prosjektliste.push({});
+prosjektliste.push({});
+prosjektliste.push({});
+prosjektliste.push({});
+prosjektliste.push({});
+prosjektliste.push({});
+prosjektliste.push({});
 
 </script>
+
+
 <style>
 .list-wrapper {
   display: flex;
