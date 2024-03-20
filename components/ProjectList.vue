6d47e8bb1ff95992088fcb1d7bedd0fc5e178653
h599017 <89259042+h599017@users.noreply.github.com>
2024-03-20 12:05:56 +0100

    begynt på knappen, blitt stuck.

--- components/ProjectList.vue
+++ components/ProjectList.vue
@@ -1,24 +1,35 @@
 <template>
-  <div class="list-wrapper" v-for="entry in prosjektliste">
-    <ProjectEntry :entryData="entry"/>
+  <div class="list-wrapper">
+    <ListDesc/>
     <hr class="solid">
+    <div v-for="entry in prosjektliste" :key="entry.id">
+      <ProjectEntry :entryData="entry"/>
+      <hr class="solid">
+    </div>
+    <button v-on:click="addItemToList">Press me</button>
   </div>
 </template>
 
-
-<script setup>
-import ProjectEntry from './ProjectEntry.vue';
-
-const prosjektliste = [];
-
-prosjektliste.push({});
-prosjektliste.push({});
-prosjektliste.push({});
-prosjektliste.push({});
-prosjektliste.push({});
-prosjektliste.push({});
-prosjektliste.push({});
-
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: "primary"
+    }
+  },
+  computed: {
+    buttonClass() {
+      return `button-${this.type}`;
+    }
+  },
+  methods: {
+    handleClick() {
+      // Emitting a custom event when the button is clicked
+      this.$emit('click');
+    }
+  }
+};
 </script>
 
 
