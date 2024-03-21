29b680b934a0bb23522e55de70e375089f5bb9cc
h599017 <89259042+h599017@users.noreply.github.com>
2024-03-21 15:11:36 +0100

    opprettet Modal

---
+++ components/Modal.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="modal_wrapper">
+    <Teleport to="body">
+      <Transition name="modal-fade"></Transition>
+    </Teleport>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.modal-fade-enter-from,
+.modal-fade-to{
+  opacity: 0;
+}
+
+.modal-fade-active,
+.modal-fade-active {
+  transition: 0.25s ease all;
+}
+
+.modal_wrapper {
+  position: fixed;
+  left: 0;
+  top: 0;
+
+  z-index: 500;
+
+  width: 100vw;
+  height: 100vh;
+
+  background: rgba(0,0,0,0.2);
+
+  display: grid;
+  place-items: center;
+}
+
+</style>
