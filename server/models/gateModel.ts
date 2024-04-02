a40601f9d10fedd83d0b880ba2ec565e8fd4af57
h599017 <89259042+h599017@users.noreply.github.com>
2024-04-02 12:46:52 +0200

    projectModel

--- server/models/gateModel.ts
+++ server/models/gateModel.ts
@@ -1,3 +1,10 @@
 class gateModel {
-    
+
+    /*Attributter*/
+
+    constructor() {
+        
+    }
+
+
 }
--- server/models/projectModel.ts
+++ server/models/projectModel.ts
@@ -1,3 +1,24 @@
 class projectModel {
-    
+
+    ID: number; /*ProsjektID*/
+    title: String; /*Tittel på prosjekt*/
+    progress: number; /*Total progresjon på prosjektet, skrives som et desimal*/
+    onTime: boolean; /*Boolean som styrer fargeknappen som indikerer prosjektets status*/
+    PEM: String; /*Project Execution Manager for et prosjekt*/
+    comment: String; /*Kommentar til prosjekt*/
+    POdate: Date; /*Dag prosjektet er bestilt ferdig innen*/
+    SFdate: Date; /*Dag prosjektet er planlagt ferdig*/
+
+
+    constructor(ID:number, title:String, progress:number, onTime:boolean, PEM:String, comment:String, POdate:Date, SFdate:Date) {
+        this.ID = ID;
+        this.title = title;
+        this.progress = progress;
+        this.onTime = onTime;
+        this.PEM = PEM;
+        this.comment = comment;
+        this.POdate = POdate;
+        this.SFdate = SFdate;
+    }
+
 }
