c7743181f08a3f1e38cdf49a613ae8a23f33c766
h599017 <89259042+h599017@users.noreply.github.com>
2024-04-02 11:11:19 +0200

    taskModel

---
+++ server/models/taskModel.ts
@@ -0,0 +1,25 @@
+class taskModel {
+
+
+    ID: number; /*ID til task*/
+    prosjektID: number; /*ID på prosjekt en task tilhører*/
+    gateID: number; /*ID på gate en task tilhører*/
+    step: number; /*Tall brukt for å sortere internt i en gate. 3.1, 3.2 osv..*/
+    title: String; /*Navn på task..*/
+    responsiblePerson: String; /*Person som utfører en endring på tasken*/
+    onTime: boolean; /*Må regnes ut hver gang*/
+    progress: number; /*Desimal som representerer prosentandel (0.25,0.5 osv...)*/
+    duration: number; /*Antall dager*/
+    
+    constructor(ID:number, prosjektID:number, gateID:number, step:number, title:String, responsiblePerson:String, onTime:boolean, progress:number, duration:number) {
+        this.ID = ID;
+        this.prosjektID = prosjektID;
+        this.gateID = gateID;
+        this.step = step;
+        this.title = title;
+        this.responsiblePerson = responsiblePerson;
+        this.onTime = onTime;
+        this.progress = progress;
+        this.duration = duration;
+    }
+}
