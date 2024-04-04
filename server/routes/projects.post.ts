e4e83a2b06b2591fc810a64233d015837b9ae416
h599017 <89259042+h599017@users.noreply.github.com>
2024-04-04 16:15:42 +0200

    Mangler bare Body

--- server/routes/projects.post.ts
+++ server/routes/projects.post.ts
@@ -1,6 +1,24 @@
 // projects.post.ts 
 export default defineEventHandler(async event => {
-  const body = await readBody (event)
 
-  return {updated: true}
+  let projects;
+  
+
+  try {
+    const body = await readBody (event)
+    const requestBody = JSON.parse(body)
+    const { title, progress, plannedDate, POdate, status, PEM, comment} = requestBody;
+
+    console.log("Attempting to create project in DB..")
+    projects = await connectAndQuery("INSERT INTO projectModel VALUES (9,'TestdalTestdalTestdal',0,1,'" + "John Doe" + "', 'Sample comment','" + "2024-10-05" + "','" + "2024-09-25" + "',0)");
+
+    return {updated: true}
+  } catch (error) {
+    return createError({
+      statusCode: 500,
+      statusMessage: 'Internal Server Error',
+      data: 'Failed to retrieve records',
+    })
+  }
+  
 })
--- stores/projects.js
+++ stores/projects.js
@@ -1,6 +1,7 @@
 import { ref, computed } from 'vue';
 import { packProject } from '@/utils/packProject.js';
 import { defineStore } from "pinia";
+import { jsx } from 'vue/jsx-runtime';
 
 export const useProjectsStore = defineStore('projects', () => {
 	// denne brukes hvis man vil hente et spesifikt object, må implementere metode for det da
@@ -16,6 +17,32 @@ export const useProjectsStore = defineStore('projects', () => {
 	}
 	// Funksjon for å legge til et prosjekt i listen
 	async function addProject(title, progress, plannedDate, PODate, status, person, comment) {
+		const requestBody = {
+			title: title,
+			progress: progress,
+			plannedDate: plannedDate,
+			PODate: PODate,
+			status: status,
+			person: person,
+			comment: comment
+		};
+
+		try {
+			const response = await $fetch('/projects', {
+				method: 'POST',
+				headers: {
+					'Content-type': 'application/json'
+				},
+				body: JSON.stringify(requestBody)
+			})
+
+		} catch (error) {
+			return createError({
+				statusCode: 500,
+				statusMessage: 'Internal Server Error',
+				data: 'Failed to create project'
+			})
+		}
 
 		this.projects.push(packProject(index.value, title, progress, plannedDate, PODate, status, person, comment))
 		index.value++;
