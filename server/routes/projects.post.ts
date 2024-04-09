87a03665df8a40dbb6efa8dbe845c1d466157b01
h599017 <89259042+h599017@users.noreply.github.com>
2024-04-09 11:00:07 +0200

    Gates

--- server/routes/projects.post.ts
+++ server/routes/projects.post.ts
@@ -6,28 +6,30 @@ export default defineEventHandler(async event => {
   
   var gateListe: any[][];
   
+  //Liste over gates som blir lagt til i opprettelsen av et prosjekt
   gateListe = [
-    [1,"navn1"], //gate 1
-    [2,"navn2"], //gate 2
-    [3,"navn3"], //gate 3
-    [4,"navn4"], //gate 4
-    [5,"navn5"], //gate 5
-    [6,"navn6"], //gate 6
-    [7,"navn7"], //gate 7
-    [8,"navn8"], //gate 8
-    [9,"navn9"], //gate 9
-    [10,"navn10"], //gate 10
-    [11,"navn11"], //gate 11
-    [12,"navn12"], //gate 12
-    [13,"navn13"], //gate 13
-    [14,"navn14"] // gate 14
+    [1,"RFQ"], //gate 1
+    [2,"QUOTATION"], //gate 2
+    [3,"PURCHASE ORDER"], //gate 3
+    [4,"DISASSEMBLY OF XT"], //gate 4
+    [5,"INSPECTION OF COMPONENTS"], //gate 5
+    [6,"3RD PARTY SERVICES"], //gate 6
+    [7,"RE-INSPECTION OF COMPONENTS AFTER 3RD PARTY"], //gate 7
+    [8,"CLEAN START REVIEW"], //gate 8
+    [9,"PRE-ASSEMBLY"], //gate 9
+    [10,"ASSEMBLY"], //gate 10
+    [11,"PRE TEST"], //gate 11
+    [12,"TEST"], //gate 12
+    [13,"POST TEST ACTIVITIES"], //gate 13
+    [14,"DELIVERY"] // gate 14
   ]
 
   try {
     const body = await readBody(event);
     const { title, progress, plannedDate, POdate, status, PEM, comment } = body;
 
-    const ID = 20;
+    const ID = 22; // Må gjøres dynamisk etter hvert.
+
     console.log("Attempting to create project in DB..");
     // Insert project into projectModel table
     projects = await connectAndQuery(`INSERT INTO projectModel VALUES (${ID}, '${title}', 0, 1, '${PEM}', 'Sample comment', '01-01-2025', '01-01-2025', 0)`);
