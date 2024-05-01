d628b782e80ae75f6e5c34b91be0d7ebbda69b3b
Kristoffer Fjeldstad Madsen <89259042+h599017@users.noreply.github.com>
2024-05-01 14:11:07 +0200

    Update gates.js

--- stores/gates.js
+++ stores/gates.js
@@ -145,7 +145,7 @@ export const useGatesStore = defineStore('gates', () => {
         });
     }
 
-    function getGateForTask(searchid) {
+    function getGateNR(searchid) {
         for(let i = 0; i<gates.value.length; i++) {
             if(searchid === Number(gates.value[i].ID)) {
                 return gates.value[i].gateNR;
@@ -155,6 +155,6 @@ export const useGatesStore = defineStore('gates', () => {
 
 
 
-    return { gates, addGate, getProjectGates, calculateDate, lastGate, substractDays, getGateProgress, fetchGates, getGateForTask };
+    return { gates, addGate, getProjectGates, calculateDate, lastGate, substractDays, getGateProgress, fetchGates, getGateNR };
 
 });
