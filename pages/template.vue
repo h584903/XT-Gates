c5aa0b0e553502103d2adc450af6bafbb51f1d8f
Petter Tesdal <h584903@stud.hvl.no>
2024-05-02 20:48:27 +0200

    Tok vekk delete template knappen

--- pages/template.vue
+++ pages/template.vue
@@ -11,15 +11,6 @@
       <div class="semi-footer">
         <SaveButton />
       </div>
-      <div>
-        <ReusableModal @close="toggleModal" :modalActive="modalActive">
-        <h1>Delete Project?</h1>
-        <p>Deleting a project is absolute, and cannot be reversed. Make certain this is necessary before doing so.</p>
-        <p>Delete project {{ project.title }}?</p>
-        <button @click="deleteProjectHandler" deleteProject class="customButton">Yes</button>
-        <button @click="toggleModal" class="customButton">No</button>
-        </ReusableModal>
-      </div>
       <button @click="toggleModal" type = "button">Delete project</button>
     </div>
     <div v-else>
