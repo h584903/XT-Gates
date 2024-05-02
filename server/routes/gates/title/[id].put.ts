6b1c13c8d6b6da5f32b9be4b1742e8e3b4723584
Eirik Sangiorgi Brakstad <596134@stud.hvl.no>
2024-05-02 20:53:15 +0200

    Kan endre gateTitle

---
+++ server/routes/gates/title/[id].put.ts
@@ -0,0 +1,26 @@
+export default defineEventHandler(async event => {
+    try {
+      // gjør klar body
+      const body = await readBody(event);
+      const { gateID, newTitle } = body;
+
+      if (!gateID || !newTitle) {
+        // Sender en error hvis requesten er feil
+        return createError({
+          statusCode: 400,
+          statusMessage: 'Bad Request',
+          data: 'gateID and newTitle must be provided',
+        });
+      }
+
+      await connectAndQuery(`UPDATE gateModel SET gateTitle = '${newTitle}' WHERE ID = ${gateID}`);
+      return { updated: true };
+    } catch (error) {
+      console.error('Failed to update the gate:', error);
+      return createError({
+        statusCode: 500,
+        statusMessage: 'Internal Server Error',
+        data: 'Failed to update the gate',
+      });
+    }
+  });
