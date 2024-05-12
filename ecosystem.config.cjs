641731654a1778300ecb8ccac9db0311f27e7c1b
Petter Tesdal <h584903@stud.hvl.no>
2024-05-12 12:42:02 +0200

    Liten fix

---
+++ ecosystem.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  apps: [
+    {
+      name: 'XT-gates',
+      port: '3000',
+      exec_mode: 'cluster',
+      instances: 'max',
+      script: './.output/server/index.mjs'
+    }
+  ]
+}
+
--- pages/projectList/[id].vue
+++ pages/projectList/[id].vue
@@ -1,5 +1,5 @@
 <template class="allWrapper">
-  <div> event id: {{ route.params.id}}
+  <div>
     <div class="wrapper" v-if="project">
       <div class="title">
         <h1>{{ project.title }}</h1>
