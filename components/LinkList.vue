5bb8a4f170bbd3792448325c6ce8ed7ea2914d8f
Eirik Sangiorgi Brakstad <596134@stud.hvl.no>
2024-05-28 14:17:33 +0200

    wallah

--- components/LinkList.vue
+++ components/LinkList.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="ProjectNameList">
     <!-- Vise listene i sidebar -->
-    <NuxtLink v-for="(project, index) in limitedProjects" :key="project.id" :to="`/projectList/${project.id}`">
+    <NuxtLink v-for="(project, index) in limitedProjects" :key="project.id" :to="`/projectList/${project.id}`" class="Thin-Line">
       {{ truncateTitle(project.title) }}
     </NuxtLink>
     <NuxtLink v-if="filteredProjects.length > 30" to="/projectList">...</NuxtLink>
@@ -43,11 +43,27 @@ const truncateTitle = (title) => {
   font-size: 20px;
 }
 
+.Thin-Line{
+  display: inline-block;
+  text-decoration: none;
+  color: inherit;
+  position: relative; 
+  padding: 2px;
+}
+.Thin-Line::after {
+  content: '';
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 1px;
+  background-color: #ccc; /* Color of the line */
+}
+
 .ProjectNameList a {
   text-decoration: none;
   color: black;
-  /* Set maximum width and allow wrapping */
-  max-width: 130px; /* Adjust the value as needed */
+  max-width: 90%;
   word-wrap: break-word;
   white-space: normal;
   display: inline-block;
