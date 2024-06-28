eb72cd43fbaa252046ce53ace5d1a1c5ff193dd2
Kristoffer Madsen <599017@stud.hvl.no>
2024-06-28 09:49:42 +0200

    team displayes rett

--- components/TeamDropdown.vue
+++ components/TeamDropdown.vue
@@ -1,18 +1,9 @@
 <template>
     <div class="dropdown">
       <div class="dropdown-selected" @click="toggleDropdown">
-        <span style="flex: 1;" v-if="teamUnedited">TEAM {{ teamStore.getTeamName(currentUserTeam) }}</span>
-        <span v-else>TEAM {{ selectedTeamName }}</span>
-        <span class="dropdown-arrow"></span>
+        <span >TEAM {{ currentUserTeamName }}</span>
       </div>
       <div class="dropdown-menu" v-if="dropdownOpen">
-        <div
-          class="dropdown-item"
-          v-if="teamUnedited"
-          @click="selectTeam(currentUserTeam)"
-        >
-          TEAM {{ teamStore.getTeamName(currentUserTeam) }}
-        </div>
         <div
           class="dropdown-item"
           v-for="team in filteredTeams"
@@ -26,58 +17,66 @@
   </template>
   
 
-<script setup>
-const projectStore = useProjectsStore();
-const teamStore = useTeamsStore();
-const authStore = useAuthStore();
-
-const teams = ref([]);
-const teamUnedited = ref(true);
-const dropdownOpen = ref(false);
-const selectedTeamName = ref('');
-
-const currentUserTeam = computed(() => authStore.getUserTeam());
-
-const filteredTeams = computed(() => {
-  const currentTeamName = teamStore.getTeamName(currentUserTeam.value);
-  return teams.value.filter(team => team.team !== currentTeamName && team.id !== 0);
-});
-
-onMounted(async () => {
-  await teamStore.fetchTeams();
-  teams.value = teamStore.getTeams();
-});
-
-async function updateUserTeam(newTeamId) {
-  authStore.setUserTeam(newTeamId);
-  await projectStore.fetchProjects();
-}
-
-function selectTeam(teamId) {
-  teamUnedited.value = false;
-  selectedTeamName.value = teamStore.getTeamName(teamId);
-  updateUserTeam(teamId);
-  toggleDropdown();
-}
-
-function toggleDropdown() {
-  dropdownOpen.value = !dropdownOpen.value;
-}
-</script>
+  <script setup>
+  const projectStore = useProjectsStore();
+  const teamStore = useTeamsStore();
+  const authStore = useAuthStore();
+  
+  let nonchecked = true
+  const teams = ref([]);
+  const teamUnedited = ref(true);
+  const dropdownOpen = ref(false);
+  
+  const currentUserTeam = computed(() => authStore.getUserTeam());
+  const currentUserTeamName = computed(() => teamStore.getTeamName(currentUserTeam.value));
+  const selectedTeamName = ref(currentUserTeamName.value);
+  
+  const filteredTeams = computed(() => {
+    const currentTeamName = teamStore.getTeamName(currentUserTeam.value);
+    return teams.value.filter(team => team.team !== currentTeamName && team.id !== 0);
+  });
+  
+  onMounted(async () => {
+    await teamStore.fetchTeams();
+    teams.value = teamStore.getTeams();
+    selectTeam(Number(currentUserTeam.value));
+    nonchecked = false
+    toggleDropdown();
+  });
+  
+  async function updateUserTeam(newTeamId) {
+    authStore.setUserTeam(newTeamId);
+    await projectStore.fetchProjects();
+  }
+  
+  function selectTeam(teamId) {
+    console.log("Set team to: " + teamId + ".");
+    console.log(typeof teamId)
+    teamUnedited.value = false;
+    selectedTeamName.value = teamStore.getTeamName(teamId);
+    updateUserTeam(teamId);
+    toggleDropdown();
+  }
+  
+  function toggleDropdown() {
+    dropdownOpen.value = !dropdownOpen.value;
+  }
+  </script>
+  
 
 
 <style scoped>
 .dropdown {
   position: relative;
-  width: 30%; /* Adjust width to make it smaller */
+  width: 35%; /* Adjust width to make it smaller */
   margin: 0 auto; /* Center the dropdown */
 }
 
 .dropdown-selected {
   padding: 8px 10px; /* Smaller padding for a smaller element */
-  border: 2px solid #000;
-  border-radius: 4px;
-  background-color: rgb(125, 253, 227);
+  border: 1px solid #000;
+  border-radius: 0px;
+  background-color: #d3e5fc;
   font-size: 30px; /* Smaller font size */
   color: #333;
   cursor: pointer;
@@ -93,19 +92,12 @@ function toggleDropdown() {
   text-align: center; /* Center text horizontally */
 }
 
-.dropdown-arrow {
-  font-size: 16px;
-  position: absolute; /* Position absolutely */
-  right: 10px; /* Adjust as necessary */
-  pointer-events: none; /* Disable pointer events */
-}
-
 .dropdown-menu {
   position: absolute;
   width: 100%;
-  border: 2px solid #000;
-  border-radius: 4px;
-  background-color: rgb(125, 253, 227);
+  border: 1px solid #000;
+  border-radius: 0px;
+  background-color: #d3e5fc;
   margin-top: 5px;
   z-index: 1000;
 }
