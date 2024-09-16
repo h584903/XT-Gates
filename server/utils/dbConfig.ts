a496905b5e408f2ead6d0517c969159949088750
installer <installer@gmail.com>
2024-09-16 12:06:15 +0200

    Lagt tilbake dbCOnfig

---
+++ server/utils/dbConfig.ts
@@ -0,0 +1,19 @@
+const runtimeConfig = useRuntimeConfig()
+
+const dbConfig = {
+  server: runtimeConfig.dbServer,
+        authentication: {
+            type: 'default',
+            options: {
+                userName: runtimeConfig.dbUser,
+                password: runtimeConfig.dbPass,
+            }
+        },
+        options: {
+            encrypt: runtimeConfig.dbEncrypt == "true",
+            database: runtimeConfig.dbName,
+            port: parseInt(runtimeConfig.dbPort as string, 10),
+        }
+};
+
+export default dbConfig;
